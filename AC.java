c04af4677da65776cc0d4016072702ce3f7e0509
Felipe428a <fe.vargasi@duocuc.cl>
2026-03-16 17:51:02 -0300

    Add files via upload

---
+++ AC.java
@@ -0,0 +1,19 @@
+package com.bibliotecaFV.Biblioteca.repository;
+import com.bibliotecaFV.Biblioteca.model.Libro;
+import org.springframework.stereotype.repository;
+
+import java.util.ArrayList;
+import java.util.list;
+
+@repository
+
+public class LibroRepository {
+
+}
+
+
+private final list<Libro> listaListo = new ArrayList<>();
+
+public List<Libro> ObtenerTodo(){
+    return listaLibro;
+}
